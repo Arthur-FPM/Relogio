e419e26aad6e9c6620df9b9ec544ec281f2fbdf9
Arthur-FPM <An4rk.social@gmail.com>
2025-11-26 20:54:50 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+function chamaRelogio() {
+    const spamRelogio = document.querySelector("#relogio")
+    const date = new Date()
+
+    const horas = date.getHours().toString().padStart(2, "0")
+
+    const minutos = date.getMinutes().toString().padStart(2, "0")
+
+    const segundos = date.getSeconds().toString().padStart(2, "0")
+
+    spamRelogio.innerHTML = `${horas}:${minutos}:${segundos}`
+
+}
+
+chamaRelogio()
+
+setInterval(chamaRelogio, 1000)
